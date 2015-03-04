5593bdfb67748754c5a0244863b5fcf1c6763f76
miloroot <milonice@hotmail.com>
2015-03-04 09:38:55 +0100

    Structuring main.js and added button classes to index.html.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,38 @@ $(function() {
 	// DOM Ready and working?
 	console.log("DOM is Ready!");
 
+	//
+	// INITIAL HIDDEN DOM-OBJECTS.
+	//
+
+	// first line of code for hidden objects here.
+
+
+
+	//
+	// BUTTON HANDLERS.
+	//
+
+	$(document).on('click', '.search', function() {
+		makeASearch();
+	});
+
+
+
+	//
+	// FUNCTIONS - GLOBAL
+	//
+
+	function makeASearch() {
+		// submit handler for the <form>, yet to come.
+	}
+
+
+
+	//
+	// INITIAL AJAX PAGELOADS.
+	//
+
+	// first line of code for init ajax loads here.
+
 });

3b8392fdd9426f3f07c32f7c8b08305071153d33
miloroot <milonice@hotmail.com>
2015-03-04 10:46:19 +0100

    .search_r and .all_r buttons working, and their ajax aswell.

--- js/main.js
+++ js/main.js
@@ -12,11 +12,15 @@ $(function() {
 
 
 	//
-	// BUTTON HANDLERS.
+	// BUTTON/SUBMIT HANDLERS.
 	//
 
-	$(document).on('click', '.search', function() {
-		makeASearch();
+	$(document).on('click', '.search_r', function() {
+		loadSearchPage();
+	});
+
+	$(document).on('click', '.all_r', function() {
+		loadAllRPage();
 	});
 
 
@@ -25,16 +29,52 @@ $(function() {
 	// FUNCTIONS - GLOBAL
 	//
 
-	function makeASearch() {
-		// submit handler for the <form>, yet to come.
+	function loadSearchPage() {
+		$.ajax({
+			url: "views/search.html",
+			dataType: "html",
+			cache: false,
+			success: function(html) {
+				$('.loadContent').html(html);
+				console.log("Loaded via .search_r button.");
+			},
+			error: function(html) {
+				console.log("Error loading the html (search_r): ", html);
+			}
+		});
+	}
+
+	function loadAllRPage() {
+		$.ajax({
+			url: "views/allrec.html",
+			dataType: "html",
+			cache: false,
+			success: function(html) {
+				$('.loadContent').html(html);
+				console.log("Loaded via .all_r button.");
+			},
+			error: function(html) {
+				console.log("Error loading the html (all_r): ", html);
+			}
+		})
 	}
 
 
 
 	//
-	// INITIAL AJAX PAGELOADS.
+	// INITIAL AJAX PAGELOAD.
 	//
 
-	// first line of code for init ajax loads here.
+	$.ajax({
+		url: "views/search.html",
+		dataType: "html",
+		cache: false,
+		success: function(html) {
+			$('.loadContent').html(html);
+		},
+		error: function(html) {
+			console.log("Error loading the html: ", html);
+		}
+	});
 
 });
